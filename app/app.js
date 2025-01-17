0e8871e1e6253301d744903ef4d4ddd97507f3ef
Jitender Singh <Jitendersinghjatt09@gmail.com>
2025-01-17 22:25:26 +0530

    cors setup done

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const cors = require('cors'); // Import the CORS package
 const app = express();
 const apiRoutes = require('./routes/api.js');
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // file for open api's mean don't need any authentication
 app.use('/pgrooms', apiRoutes);
-
 
 module.exports = app;
