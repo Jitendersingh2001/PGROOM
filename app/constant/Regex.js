56ac0ef7c7e774cb1c47da1c7e681f22b1f0ef74
jitender singh <jitendersinghjatt09@gmail.com>
2025-03-01 22:36:16 +0530

    add base64 validation for propertyImage in propertyValidator and improve regex constants

--- app/constant/Regex.js
+++ app/constant/Regex.js
@@ -1,5 +1,6 @@
 const RegExp = {
-    PASSWORD_REGEX : '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+    PASSWORD_REGEX: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$',
+    BASE64_REGEX: '^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$'
 };
 
 module.exports = RegExp;
--- app/validators/propertyValidator.js
+++ app/validators/propertyValidator.js
@@ -1,33 +1,36 @@
-const Joi = require('joi');
+const Joi = require("joi");
+const regex = require("../constant/Regex");
 
 const propertyValidator = Joi.object({
   state: Joi.string()
     .required()
-    .empty(['', null])
+    .empty(["", null])
     .messages({
-      'any.required': 'State is required',
-      'string.base': 'State must be a string',
+      "any.required": "State is required",
+      "string.base": "State must be a string",
     }),
   city: Joi.string()
     .required()
-    .empty(['', null])
+    .empty(["", null])
     .messages({
-      'any.required': 'City is required',
-      'string.base': 'City must be a string',
+      "any.required": "City is required",
+      "string.base": "City must be a string",
     }),
   propertyName: Joi.string()
     .required()
-    .empty(['', null])
+    .empty(["", null])
     .messages({
-      'any.required': 'Property Name is required',
-      'string.base': 'Property Name must be a string',
+      "any.required": "Property Name is required",
+      "string.base": "Property Name must be a string",
     }),
   propertyImage: Joi.string()
     .required()
-    .empty(['', null])
+    .empty(["", null])
+    .pattern(new RegExp(regex.base64))
     .messages({
-      'any.required': 'Image is required',
-      'string.base': 'Image must be a string',
+      "any.required": "Image is required",
+      "string.base": "Image must be a string",
+      "string.pattern.base": "Image must be a valid Base64 string",
     }),
 });
 
