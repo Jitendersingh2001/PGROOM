9f05fbbd646bce0cc1fca2877f48e76b932629d2
jitender singh <jitendersinghjatt09@gmail.com>
2025-03-02 16:02:09 +0530

    fix the format

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const propertyController = require('../controllers/propertyController'); 
+const propertyController = require('../controllers/propertyController');
 const imageController = require('../controllers/imageController');
 const validators = require("../validators/index");
 const validateRequest = require("../middleware/validationMiddleware");
@@ -9,19 +9,29 @@ const upload = require("../middleware/multerMiddleware");
 /**
  * Property routes
  */
- router.post('/addproperty', upload.array("images", 10) , validateRequest(validators.propertyValidator), propertyController.addProperty);
-  
-  router.get('/getproperty', (req, res) => {
-    res.send('Get property');
-  });
-  
-  router.put('/updateproperty', (req, res) => {
-    res.send('Update property');
-  });
-  
-  router.delete('/deleteproperty', (req, res) => {
-    res.send('Delete property');
-  });
+router.post(
+  '/addproperty',
+  upload.array("images", 10),
+  validateRequest(validators.propertyValidator),
+  propertyController.addProperty
+);
 
-  router.post('/uploadImage', upload.array("images", 10), imageController.uploadImage);
+router.get('/getproperty', (req, res) => {
+  res.send('Get property');
+});
+
+router.put('/updateproperty', (req, res) => {
+  res.send('Update property');
+});
+
+router.delete('/deleteproperty', (req, res) => {
+  res.send('Delete property');
+});
+
+router.post(
+  '/uploadImage',
+  upload.array("images", 10),
+  imageController.uploadImage
+);
+
 module.exports = router;
