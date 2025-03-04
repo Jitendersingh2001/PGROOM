bc3b4e056dca6ae80f2c8202f3c1a89c7ccff8c7
jitender singh <jitendersinghjatt09@gmail.com>
2025-03-04 22:44:41 +0530

    add propertyAddress and propertyContact fields to UserProperties model and migration

--- important_cmmds.js
+++ important_cmmds.js
@@ -1,5 +1,6 @@
 /**
  * docker run --name my_postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin123 -e POSTGRES_DB=pgrooms -p 5432:5432 -d postgres
+ * to create migration: - npx prisma migrate dev --create-only
  * to run the migrations : - npx prisma migrate dev --name init
  * Open Prisma Studio to view the database: - npx prisma studio
  * 
