d077838f9fb18e6f1669638ca4365373a1335c5e
jitender singh <jitendersinghjatt09@gmail.com>
2025-03-10 12:44:17 +0530

    refactor property routes for improved readability and organization; consolidate route definitions and enhance code structure

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -1,39 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const propertyController = require('../controllers/propertyController');
-const imageController = require('../controllers/imageController');
+const propertyController = require("../controllers/propertyController");
+const imageController = require("../controllers/imageController");
 const validators = require("../validators/index");
 const validateRequest = require("../middleware/validationMiddleware");
 const upload = require("../middleware/multerMiddleware");
 
+const uploadImages = upload.array("images", 10);
+
 /**
- * Property routes
+ * Property Routes
  */
-router.post(
-  '/property',
-  upload.array("images", 10),
-  validateRequest(validators.propertyValidator),
-  propertyController.addProperty
-);
+router
+  .route("/property")
+  .post(
+    uploadImages,
+    validateRequest(validators.propertyValidator),
+    propertyController.addProperty
+  )
+  .put(
+    uploadImages,
+    validateRequest(validators.propertyValidator),
+    propertyController.updateProperty
+  );
 
-router.get('/property/:id',
-  propertyController.getProperty
-);
+router
+  .route("/property/:id")
+  .get(propertyController.getProperty)
+  .delete(propertyController.deleteProperty);
 
-router.put('/property',
-  upload.array("images", 10),
-  validateRequest(validators.propertyValidator),
-  propertyController.updateProperty
-);
-
-router.delete('/property/:id',
-  propertyController.deleteProperty
-);
-
-router.post(
-  '/uploadImage',
-  upload.array("images", 10),
-  imageController.uploadImage
-);
+/**
+ * Image Routes
+ */
+router.post("/uploadImage", uploadImages, imageController.uploadImage);
 
 module.exports = router;
