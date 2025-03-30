75d6f75ce97f954ad7e373d1b01f5bd527f06961
jitender singh <jitendersinghjatt09@gmail.com>
2025-03-30 12:35:27 +0530

    feat(user): enhance getUsersByRoleId to support additional dynamic columns in selection

--- app/repository/userRepository.js
+++ app/repository/userRepository.js
@@ -8,40 +8,62 @@ class userRepository {
     this.prisma = prismaClient || new PrismaClient();
   }
 
-    
-    /**
-     * function to get user ids by role id
-     */
-    async getUsersByRoleId(roleId, searchInput, searchFields, page, limit) {
-        try {
-            const searchConditions = searchInput
-            ? searchFields.map(field => ({ [field]: { contains: searchInput, mode: 'insensitive' } }))
-            : undefined;
-            const queryOptions = {
-                where: {
-                    roleId: roleId,
-                    user: {
-                        status: constant.ACTIVE,
-                        OR: searchConditions,
-                    },
-                },
-                select: {
-                    user: {
-                        select: {
-                            id: true,
-                            firstName: true,
-                            lastName: true,
-                            email: true,
-                            status: true,
-                        }
-                    }
-                },
-            };
-            return await paginate(this.prisma.userRoleLink, queryOptions, page, limit)
-        } catch (error) {
-            throw error;
-        }
-    }    
+  /**
+   * function to get user ids by role id
+   */
+  async getUsersByRoleId(
+    roleId,
+    searchInput,
+    searchFields,
+    page,
+    limit,
+    additionalColumns = []
+  ) {
+    try {
+      const searchConditions = searchInput
+        ? searchFields.map((field) => ({
+            [field]: { contains: searchInput, mode: "insensitive" },
+          }))
+        : undefined;
+      const defaultColumns = {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        status: true,
+      };
+      // Merge default columns with additional ones
+      const selectedColumns = additionalColumns.reduce(
+        (acc, column) => {
+          acc[column] = true;
+          return acc;
+        },
+        { ...defaultColumns }
+      );
+      const queryOptions = {
+        where: {
+          roleId: roleId,
+          user: {
+            status: constant.ACTIVE,
+            OR: searchConditions,
+          },
+        },
+        select: {
+          user: {
+            select: selectedColumns,
+          },
+        },
+      };
+      return await paginate(
+        this.prisma.userRoleLink,
+        queryOptions,
+        page,
+        limit
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 module.exports = new userRepository();
